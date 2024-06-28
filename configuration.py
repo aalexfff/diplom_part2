dec989d972c074526ce4974ac288e36847ec37f1
aalexfff <avocaddofff@yandex.ru>
2024-06-28 14:36:36 +0300

    Сonfiguration.py

--- configuration.py
+++ configuration.py
@@ -1,18 +1,5 @@
-URL_SERVICE = 'https://580422bb-7d0c-4fd1-8144-4c792f8d1bfd.serverhub.praktikum-services.ru'
+URL_SERVICE = 'https://fe2e48a1-4b50-4c85-b76b-6c7718691100.serverhub.praktikum-services.ru'
 CREATE_ORDER ="/api/v1/orders" #создание заказа
 GET_ORDER ="/api/v1/orders/?t=" #получение заказа по его номеру
 
-#Тело для создания заказа
-order_body = {
-    "firstName": "Naruto",
-    "lastName": "Uchiha",
-    "address": "Konoha, 142 apt.",
-    "metroStation": 4,
-    "phone": "+7 800 355 35 35",
-    "rentTime": 5,
-    "deliveryDate": "2020-06-06",
-    "comment": "Saske, come back to Konoha",
-    "color": [
-        "BLACK"
-    ]
-}
+
